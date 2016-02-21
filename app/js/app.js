ec09a982fb3b2ba9d74f9c045f7a38c2a0dad13c
Nevin Madhukar K <nevinm@qburst.com>
2016-02-21 21:58:03 +0530

    Set link to the index page.

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,26 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+var routerApp = angular.module('myApp', [
     'ui.router',
     'myApp.filters',
     'myApp.services',
     'myApp.directives',
     'myApp.controllers'
-])
+]);
+
+routerApp.config(function($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider
+
+	.state('home',{
+		url:"/home",
+		templateUrl: 'partials/partial-home.html'
+	})
+
+	.state('about',{
+		
+	})
+});
