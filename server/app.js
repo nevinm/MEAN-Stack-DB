ce781051215615c8a1b8087af7de7cdf186d00cf
Nevin <nevinm@qburst.com>
2016-03-10 11:53:02 +0530

    Server files.

---
+++ server/app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+// var routes = require('routes');
+var http = require('http');
+var path = require('path');
+var mongoose = require('mongoose');
+var app = express();
+var Book = require('./Book.model');
+var db = mongoose.connection;
+var port = 3000;
+
+console.log("Hello there fellow human, please check http://localhost:" + port + "/");
+
+app.set('port', process.env.PORT || port);
+app.set('views', path.join(__dirname, 'views'));
+
+mongoose.connect("mongodb://localhost/books");
+
+//Checking the database connectivity.
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Houston, we have lift off for database!");
+});
+
+//Basic home page routing
+app.get('/', function(req, res) {
+    res.send("Happy to be here");
+});
+
+//Getting the books
+app.get('/books', function(req, res) {
+    console.log("Getting all books")
+    Book.find({})
+        .exec(function(err, books) {
+            if (err) {
+                res.send("Error has occured");
+            } else {
+            	console.log(books)
+                res.json(books);
+            }
+        })
+});
+
+
+app.listen(port);
